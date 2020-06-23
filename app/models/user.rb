89a64c925b600a87fc48d9b175f952030ece96d4
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-23 20:19:09 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+
+  def mood
+    if !happiness.nil?
+      if happiness > nausea
+        "happy"
+      else
+        "sad"
+      end
+   end
+ end
+
 end
