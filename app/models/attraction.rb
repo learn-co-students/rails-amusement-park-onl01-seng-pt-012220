a25b0c1357ca4367da0260f2982073ef3c385d4b
haileybongo <haileyebongo@gmail.com>
2020-07-06 17:48:50 -0700

    Done.

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,2 +1,11 @@
 class Attraction < ApplicationRecord
+    has_many :rides
+    has_many :users, through: :rides
+
 end
+
+# :name => "Roller Coaster",
+# :tickets => 5,
+# :nausea_rating => 2,
+# :happiness_rating => 4,
+# :min_height => 32
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :users
+resources :attractions
+root 'users#welcome'
+get '/signup' => 'users#new'
+get '/signin' => 'users#signin', as: 'login'
+post '/signin' => 'users#login'
+get '/logout' => 'users#logout', as: 'logout'
+post '/attractions/new' => 'attractions/new'
+post '/ride' => 'attractions#ride' 
 end
